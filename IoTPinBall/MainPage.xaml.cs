b6a5ef2fcdf29927c0128d5fed74113ddd267fdc
Lee <leealanberg@gmail.com>
2016-01-27 17:49:16 -0600

    Wireframing some basic logic and text file score logging

--- IoTPinBall/MainPage.xaml.cs
+++ IoTPinBall/MainPage.xaml.cs
@@ -31,32 +31,74 @@ namespace IoTPinBall
 
         public int score;
         public string currentPlayer;
+        public DateTime GameStartDateTime;
+        public DateTime GameStopDateTime;
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
             currentPlayer = TextBoxPlayerName.Text;
+            GameStartDateTime = DateTime.Now;
+
             PlayerSetup.Visibility = Visibility.Collapsed;
             GamePlay.Visibility = Visibility.Visible;
+
         }
 
 
         private void button_AddPoints_Click(object sender, RoutedEventArgs e)
         {
-            score = score + 100;
+
+            Random random = new Random();
+            int randomNumber = random.Next(0, 1000);
+            score = score + randomNumber;
             TextBox_CURRENTSCORE.Text = score.ToString();
+
+        }
+
+
+        private async void Write_Scores()
+        {
+
+            //Write Local / Bakcup data to Text
+         
+
+
+            Windows.Storage.StorageFolder storageFolder =    Windows.Storage.ApplicationData.Current.LocalFolder;
+            //Windows.Storage.StorageFile sampleFile = await storageFolder.CreateFileAsync("Scores.txt");
+            Windows.Storage.StorageFile sampleFile = await storageFolder.GetFileAsync("Scores.txt");
+
+            System.Diagnostics.Debug.WriteLine("Where the F is the data? : " + storageFolder.Path.ToString());
+
+
+            System.Diagnostics.Debug.WriteLine("Player: " + currentPlayer + " - SCORE:" + score + " - START: " + GameStartDateTime + " - STOP:" + GameStopDateTime);
+            await Windows.Storage.FileIO.AppendTextAsync(sampleFile, currentPlayer + "," + score + "," + GameStartDateTime + "," + GameStopDateTime + System.Environment.NewLine);
+
+
+            //Write Real Data to IoT
+
+
+
 
         }
 
         private void button_GameDone_Click(object sender, RoutedEventArgs e)
         {
+
+            GameStopDateTime = DateTime.Now;
+            Write_Scores();
+       
             PlayerSetup.Visibility = Visibility.Collapsed;
             GamePlay.Visibility = Visibility.Collapsed;
             StackGameover.Visibility = Visibility.Visible;
+
+
+
         }
 
         private void button_reset_Click(object sender, RoutedEventArgs e)
         {
             score = 0;
+            TextBox_CURRENTSCORE.Text = "";
             currentPlayer = "";
             TextBoxPlayerName.Text = "";
 
